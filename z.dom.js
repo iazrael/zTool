3a0e97f352ea0f87809aa7ebac96bdc77befd815
azrael <azrael@imatlas.com>
2011-12-29 11:25:36 +0800

    拆分dom模块
    Signed-off-by: azrael <azrael@imatlas.com>

---
+++ z.dom.js
@@ -0,0 +1,33 @@
+Z.$package('Z.dom', function(z){
+    
+    this.ellipsis = function(element){
+        var limitWidth = element.clientWidth;
+        var ellipsisText = '…';
+        var temp = element.cloneNode(true);
+        temp.id = 'checkTextLengthNode';
+        temp.className = 'check-text-length-node';
+        element.parentNode.appendChild(temp);
+        var realWidth = temp.clientWidth;
+        if(realWidth <= limitWidth){
+            return;
+        }
+        temp.innerHTML = ellipsisText;
+        var elliWidth = temp.clientWidth;
+
+        var str = element.innerHTML;
+        str = str.replace(/\s+/g, ' ');
+        var s, totalWidth = 0;
+        for(var i = 0, len = str.length; i < len; i++){
+            s = str.charAt(i);
+            temp.innerHTML = (s === ' ' ? '&nbsp;' : s);
+            totalWidth += temp.clientWidth ;
+            if(totalWidth + elliWidth > limitWidth){
+                str = str.substr(0, i);
+                break;
+            }
+        }
+        element.innerHTML = str + ellipsisText;
+        temp.parentNode.removeChild(temp);
+    }
+    
+});
