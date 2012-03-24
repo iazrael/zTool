ec95f41f37c6720266227f35642b9a9c00c1c1a9
azrael <azrael@imatlas.com>
2012-03-24 15:05:01 +0800

    修改 array.remove 的实现, 根据指定key和value删除

--- z.array.js
+++ z.array.js
@@ -1,16 +1,33 @@
 
 ;Z.$package('Z.array', function(z){
     
-    this.remove = function(arr, item){
-        //TODO 可以重构
+    /**
+     * 从给定数组移除指定元素, 只删除一个
+     * @param  {Array} arr  
+     * @param  {Object},{String} key
+     * @param {Object} value @optional 指定值
+     * @return {Boolean}      找到并移除返回 true
+     */
+    this.remove = function(arr, key, value){
         var flag = false;
-        for(var i = 0, len = arr.length; i < len; i++){
-            if(arr[i] === item){
-                arr.splice(i, 1);
+        if(arguments.length === 2){//两个参数
+            var item = key;
+            var index = arr.indexOf(item);
+            if(index !== -1){
+                arr.splice(index, 1);
                 flag = true;
             }
+            return flag;
+        }else{
+            for(var i = 0, len = arr.length; i < len; i++){
+                if(arr[i][key] === value){
+                    arr.splice(i, 1);
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
         }
-        return flag;
     };
 
     /**
