28be0a0885ed3dd98ee6f6bcd91d1867084743eb
azrael <azrael@imatlas.com>
2013-03-23 11:21:01 +0800

    去掉落伍的template方法

--- src/z.string.js
+++ src/z.string.js
@@ -23,62 +23,6 @@
         return result.join("&");
     };
     
-    var templateCache = {};
-      
-    /**
-     * 多行或单行字符串模板处理
-     * 
-     * @method template
-     * @memberOf string
-     * 
-     * @param {String} str 模板字符串
-     * @param {Object} obj 要套入的数据对象
-     * @return {String} 返回与数据对象合成后的字符串
-     * 
-     * @example
-     * <script type="text/html" id="user_tmpl">
-     *   <% for ( var i = 0; i < users.length; i++ ) { %>
-     *     <li><a href="<%=users[i].url%>"><%=users[i].name%></a></li>
-     *   <% } %>
-     * </script>
-     * 
-     * Jx().$package(function(J){
-     *  // 用 obj 对象的数据合并到字符串模板中
-     *  J.template("Hello, {name}!", {
-     *      name:"Kinvix"
-     *  });
-     * };
-     */
-    var template = this.template = function(str, data){
-        // Figure out if we're getting a template, or if we need to
-        // load the template - and be sure to cache the result.
-        var fn = !/\W/.test(str) ?
-          templateCache[str] = templateCache[str] ||
-            template(document.getElementById(str).innerHTML) :
-          
-          // Generate a reusable function that will serve as a template
-          // generator (and which will be cached).
-          new Function("obj",
-            "var z_tmp=[],print=function(){z_tmp.push.apply(z_tmp,arguments);};" +
-            
-            // Introduce the data as local variables using with(){}
-            "with(obj){z_tmp.push('" +
-            
-            // Convert the template into pure JavaScript
-            str
-              .replace(/[\r\t\n]/g, " ")
-              .split("<%").join("\t")
-              .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-              .replace(/\t=(.*?)%>/g, "',$1,'")
-              .split("\t").join("');")
-              .split("%>").join("z_tmp.push('")
-              .split("\r").join("\\'")
-          + "');}return z_tmp.join('');");
-        
-        // Provide some basic currying to the user
-        return data ? fn( data ) : fn;
-    };
-    
     /**
      * 字符串格式函数
      * 
@@ -150,4 +94,5 @@
         }
         return total;
     }
+
 });
